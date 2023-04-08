59a0575409ebe65588a4b237d16d3ac22d24175f
yoonms0617 <yoonminwater@gmail.com>
2023-04-08 22:38:39 +0900

    chore: prettier 줄바꿈 길이 변경 80 → 250

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,16 +3,18 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        printWidth: 250,
+      },
+    ],
   },
 };
