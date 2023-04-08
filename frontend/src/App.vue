2c148faecc3afed7bd10e7b1a5d10e315c07994c
yoonms0617 <yoonminwater@gmail.com>
2023-04-08 22:42:09 +0900

    design: LINESeedKR-Bd 폰트 적용

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view />
 </template>
 
+<script>
+export default {
+  name: "App",
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@font-face {
+  font-family: "LINESeedKR-Bd";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2") format("woff2");
+  font-weight: 700;
+  font-style: normal;
 }
 
-nav {
-  padding: 30px;
+* {
+  font-family: "LINESeedKR-Bd", serif;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+input {
+  box-shadow: none !important;
 }
 </style>
