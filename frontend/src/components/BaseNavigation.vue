351394151fa78b4c89eff0e4dcd0ecce08e926c2
yoonms0617 <yoonminwater@gmail.com>
2023-04-08 22:39:51 +0900

    dsign: 공통 네비게이션 컴포넌트 추가

---
+++ frontend/src/components/BaseNavigation.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <header class="d-flex justify-content-center py-3 mb-4 border-bottom">
+      <router-link to="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <span class="fs-4">게시판</span>
+      </router-link>
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link">로그인</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/signup" class="nav-link">회원가입</router-link>
+        </li>
+      </ul>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseNavigation",
+};
+</script>
