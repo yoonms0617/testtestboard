774695aba53c1175922dad1a74977ba673342842
Yoon <yoonminwater@gmail.com>
2023-04-09 21:18:35 +0900

    design: 사용자 로그인 상태에 따른 네비게이션 메뉴 처리 (#25)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,19 @@ import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/member/LoginView.vue";
 import SignupView from "@/views/member/SignupView.vue";
 
+const ACCESS_TOKEN_KEY = process.env.VUE_APP_ACCESS_TOKEN_KEY;
+
+function isLogin(to, from, next) {
+  const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY);
+  if (accessToken) {
+    next({
+      name: "home",
+    });
+  } else {
+    next();
+  }
+}
+
 const routes = [
   {
     path: "/",
@@ -13,11 +26,13 @@ const routes = [
     path: "/login",
     name: "login",
     component: LoginView,
+    beforeEnter: isLogin,
   },
   {
     path: "/signup",
     name: "signup",
     component: SignupView,
+    beforeEnter: isLogin,
   },
 ];
 
